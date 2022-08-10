37bf26b8707cb65850ca5b3f17744a7a9c53fa20
Tejaswini Gajbhiye <40845324+Teju-g49@users.noreply.github.com>
2022-08-10 13:28:09 +0530

    Update State.js

--- src/components/State.js
+++ src/components/State.js
@@ -1,35 +1,30 @@
-import React, { useState } from 'react';
+import React,{useState} from 'react';
 import City from './City';
 
-const State = ({ state, id }) => {
-        const [toggle, setToggle] = useState(false);
+const State = ({state,id}) => {
+    const[toggle,setToggle] = useState(false);
 
-        function handleToggle() {
-            if (!toggle) {
-                setToggle(true);
-            } else {
-                setToggle(false);
-            }
+    function handleToggle () {
+        if(!toggle) {
+            setToggle(true);
         }
-        return ( <
-            >
-            <
-            li id = { id }
-            onClick = { handleToggle } > { state.name } <
-            /li>
-
-            {
-                toggle && < ul > {
-                        state.cities.map((city, id) => {
-                            return <City city = { city }
-                            id = { 'city' + (id + 1) }
-                            key = { id }
-                            />
-                        })
-                    } <
-                    /ul>} <
-                    />
-            )
+        else {
+            setToggle(false);
         }
+    }
+  return (
+    <>
+      <li id={id} onClick={handleToggle}>
+        {state.name}
+      </li>
 
-        export default State
+        {toggle && <ul>
+            {state.cities.map((city,id)=> {
+                return <City city={city} id={'city'+(id+1)} key={id}/>
+            })}
+        </ul>}
+    </>
+  )
+}
+
+export default State
