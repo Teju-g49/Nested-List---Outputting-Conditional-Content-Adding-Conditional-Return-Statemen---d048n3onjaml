b19abe0f972293e785eb3631a86cb9eedc107cac
Tejaswini Gajbhiye <40845324+Teju-g49@users.noreply.github.com>
2022-08-10 13:23:32 +0530

    tejas

---
+++ src/components/State.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import City from './City';
+
+const State = ({ state, id }) => {
+        const [toggle, setToggle] = useState(false);
+
+        function handleToggle() {
+            if (!toggle) {
+                setToggle(true);
+            } else {
+                setToggle(false);
+            }
+        }
+        return ( <
+            >
+            <
+            li id = { id }
+            onClick = { handleToggle } > { state.name } <
+            /li>
+
+            {
+                toggle && < ul > {
+                        state.cities.map((city, id) => {
+                            return <City city = { city }
+                            id = { 'city' + (id + 1) }
+                            key = { id }
+                            />
+                        })
+                    } <
+                    /ul>} <
+                    />
+            )
+        }
+
+        export default State
