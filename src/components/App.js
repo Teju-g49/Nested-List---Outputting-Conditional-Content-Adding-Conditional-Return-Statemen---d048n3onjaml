6850a628a982200c47fb40c3b6d645c00172711b
Tejaswini Gajbhiye <40845324+Teju-g49@users.noreply.github.com>
2022-08-10 13:26:32 +0530

    Update App.js

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component, useState } from "react";
+import "./../styles/App.css";
+import states from "./data";
+import State from "./State";
+
+
+// Do not alter the states const and values inside it.
+
+
+function App() {
+  
+  return (
+    <div id="main">
+      <ul>
+      {states.map((state,id)=> {
+        return <State state={state} id={'state'+(id+1)} key={id}/>
+      }) } 
+      </ul>
+    </div>
+  )
+}
+
+export default App;
